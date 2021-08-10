84eca873f76457123947d8528037716907f0e914
XQ <qiux0518@hotmail.com>
2021-08-09 17:45:21 -0700

    Add crl (#1)
    Add CRL checks

--- cmd/client.go
+++ cmd/client.go
@@ -16,11 +16,10 @@ limitations under the License.
 package cmd
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/tls"
 	"crypto/x509"
-	"encoding/base64"
+	"crypto/x509/pkix"
 	"errors"
 	"fmt"
 	"github.com/google/go-cmp/cmp"
@@ -46,7 +45,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: connectAndVerify,
+	Run: connectAndValidate,
 }
 
 func init() {
@@ -56,11 +55,9 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	clientCmd.PersistentFlags().StringVar(&hostPort, "host", "", "host:port")
-	clientCmd.PersistentFlags().StringVarP(&proxyStr, "proxy", "x", "", "scheme://(user):(password)@host:port")
-	clientCmd.PersistentFlags().StringVar(&CAFile, "cafile", "", "CAfile used to verify the OCSP response")
-	clientCmd.PersistentFlags().StringVar(&CAPath, "capath", "", "Directoray containing CAfile used to verify the OCSP response")
-	clientCmd.PersistentFlags().BoolVar(&dryRun, "dry", false, "Dry run")
+	clientCmd.PersistentFlags().StringVar(&server, "server", "", "host:port")
+	clientCmd.PersistentFlags().StringVarP(&proxy, "proxy", "x", "", "scheme://(user):(password)@host:port")
+	clientCmd.PersistentFlags().StringVar(&CAFile, "cafile", "", "CAfile used to verify the cert chain")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
@@ -68,134 +65,57 @@ func init() {
 }
 
 var (
-	hostPort	string
-	proxyStr 	string
-	CAFile		string
-	CAPath		string
-	dryRun		bool
+	server	string
+	proxy 	string
+	CAFile	string
 
-	// ErrOCSPNotVerified cannot be retried
-	ErrOCSPNotVerified = errors.New("unable to verify any certificate chain")
+	ErrNotValidated = errors.New("unable to validate any cert chain")
 
-	// ErrProxyNotAvailable cannot be retried
-	ErrProxyNotAvailable = errors.New("unable to establish connection to proxy")
 )
 
 // ProxyFunc is used to simplify proxy detection and setting proxies in the client.
 type ProxyFunc func(*http.Request) (*url.URL, error)
 
-const maxConAttempts = 5
-const ocspSoftFailTimeOut = 5 * time.Second
-const ocspHardFailTimeOut = 15 * time.Second
+const gocspTimeOut = 10 * time.Second
 
-// ConnectAndVerify does 2 things:
-// 1. Connect to host, initiate the ssl handshake process to get the cert chain from server which triggers a callback function where we examine the revocation status of each cert
-// 2. Goes over each cert, sends an OCSP request to the corresponding OCSP server to check the revocation status
-func connectAndVerify(cmd *cobra.Command, args []string) {
+// ConnectAndValidate does the following things:
+// 1. Connect to host, initiate the ssl handshake process to get the cert chain from server
+// 2. Triggers a callback function where we examine the revocation status of each cert
+// 3. Goes over each cert, check its CRL or OCSP status
+func connectAndValidate(cmd *cobra.Command, args []string) {
 
 	// hostPort is not expected to have a scheme, so do not use url.Parse()
-	host, port, err := net.SplitHostPort(hostPort)
+	serverHost, serverPort, err := net.SplitHostPort(server)
 	if err != nil{
-		log.Fatalf("Error parsing host: %s.\n", err.Error())
+		log.Fatalf("Error parsing server: %s.\n", err.Error())
 	}
 
-	proxyURL, err := url.Parse(proxyStr)
-	if err != nil{
-		log.Fatalf("Error parsing proxy: %s.\n", err.Error())
+	// Parse proxy. This logic is needed because url.Parse and url.ParseRequestURI do not return nil for empty string.
+	var proxyURL *url.URL
+	if proxy == ""{
+		proxyURL = nil
+	}else{
+		proxyURL, err = url.ParseRequestURI(proxy)
+		if err != nil{
+			log.Fatalf("Error parsing proxy: %s.\n", err.Error())
+		}
 	}
 
-	var proxyHostPort = proxyURL.Host
-	var proxyFunc ProxyFunc
-	if proxyHostPort != ""{
-		proxyFunc = http.ProxyURL(proxyURL)
-	} else{
-		proxyFunc = nil		// http.ProxyURL is not smart enough to do this on an empty string
-	}
-
-	rootCAs, err := util.LoadVerifyLocations(CAFile, CAPath)
-	if err != nil {
-		log.Printf("Error loading custom CA: %s. Using system default CAs instead.\n", err.Error())
-		// If RootCAs is nil, TLS uses the host's root CA set
-		rootCAs = nil
-	}
-
+	// custom CA
+	CA, err := util.LoadCA(CAFile)
 	var tlsConfig = &tls.Config{
 		VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
-			return checkOCSP(proxyFunc, dryRun, verifiedChains)
+			return checkRevocation(proxyURL, verifiedChains)
 		},
-		RootCAs:    rootCAs,
-		ServerName: host, 	// google.com
+		RootCAs:    CA,
+		ServerName: serverHost,
 	}
 
-	// handshake with the host to trigger the callback
-	attempt := 0
-	fib := util.Fibonacci()
-
-	for attempt < maxConAttempts {
-		if proxyHostPort != "" {
-			err = handshakeProxy(host, port, proxyURL, tlsConfig)
-		} else {
-			_, err = tls.Dial("tcp", fmt.Sprintf("%s:%s", host, port), tlsConfig)
-		}
-
-		// If if's an OCSP error, we return immediately, as retrying would not change the outcome
-		if err == ErrOCSPNotVerified || err == ErrProxyNotAvailable {
-			log.Fatalf("Irrecoverable error: %s.\n", err.Error())
-		}
-
-		if err == nil{
-			return
-		}
-		// otherwise, it's possible that KGP server is not ready yet. Retries handshake
-		log.Printf("Warning: Something went wrong before we reached the OCSP step: ", err, ". This may be recoverable, retrying...\n")
-		sleep := fib() // 1, 1, 2, 3, 5, etc
-		log.Printf("Sleeping for ", sleep, " seconds.\n")
-		time.Sleep(time.Duration(sleep) * time.Second)
-		attempt++
-	}
-	return
-}
-
-// Initiate the ssl handshake through a proxy
-func handshakeProxy(host string, port string, proxyURL *url.URL, tlsConfig *tls.Config) (err error) {
-	conn, err := net.Dial("tcp", proxyURL.Host)
-	if err != nil {
-		return
-	}
-	defer conn.Close()
-
-	req, err := http.NewRequest("CONNECT", fmt.Sprintf("https://%s:%s", host, port), nil)
-	if err != nil {
-		return
-	}
-
-	if proxyURL.User.String() != "" {
-		basicAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(proxyURL.User.String()))
-		req.Header.Add("Proxy-Authorization", basicAuth)
-	}
-
-	err = req.Write(conn)
-	if err != nil {
-		return
-	}
-
-	resp, err := http.ReadResponse(bufio.NewReader(conn), req)
-	if err != nil {
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		log.Printf("non-200 response status from proxy server: ", resp.Status, "\n")
-		return ErrProxyNotAvailable
-	}
-	log.Println("Succesfully established connection to proxy")
-	tlsConn := tls.Client(conn, tlsConfig)
-	defer tlsConn.Close()
-
-	err = tlsConn.Handshake() // implicitly calls checkOCSP()
-	if err != nil {
-		return
+	// Initiate TLS handshake to trigger the checkRevocation callback
+	if proxyURL != nil{
+		err = util.HandshakeProxy(fmt.Sprintf("%s:%s", serverHost, serverPort), proxyURL, tlsConfig)
+	} else {
+		_, err = tls.Dial("tcp", fmt.Sprintf("%s:%s", serverHost, serverPort), tlsConfig)
 	}
 
 	return
@@ -206,22 +126,22 @@ func handshakeProxy(host string, port string, proxyURL *url.URL, tlsConfig *tls.
 // If normal verification fails(wrong hostname, cert expired, etc), the handshake process aborts and the callback function would not be invoked.
 // rawCerts: the cert chain that's sent from the peer.
 // verifiedChains: the result of applying x509.Certificate.Verify() on rawCerts.
-func checkOCSP(proxyFunc ProxyFunc, dryRun bool, verifiedChains [][]*x509.Certificate) (err error) {
-	log.Println("Starting OCSP certificate check.")
+func checkRevocation(proxyURL *url.URL, verifiedChains [][]*x509.Certificate) (err error) {
+	log.Println("Starting certificate validation.")
 
 	for _, chain := range verifiedChains {
 		for i, cert := range chain {
 			var nextCert *x509.Certificate
 
 			certName := fmt.Sprintf("Cert #%d", i+1)
-			log.Printf("%s: Checking OCSP status.\n", certName)
+			log.Printf("%s: Checking revocation status.\n", certName)
 			log.Printf("%s Subject: %s\n", certName, cert.Subject)
 			log.Printf("%s Issuer: %s\n", certName, cert.Issuer)
 
 			if i+1 < len(chain) {
 				nextCert = chain[i+1]
 			} else if cert.IsCA {
-				log.Println("Reached a trusted CA. Certificate chain is verified.")
+				log.Println("Reached a trusted CA. Certificate chain is validated.")
 				return nil // we found one good chain. This is the only place a good cert chain should return
 			} else {
 				log.Println("Warning: CA certificate is missing from the current certificate chain. Trying the next chain if there is one.")
@@ -233,91 +153,134 @@ func checkOCSP(proxyFunc ProxyFunc, dryRun bool, verifiedChains [][]*x509.Certif
 				break
 			}
 
-			err = isCertRevoked(cert, nextCert, proxyFunc, dryRun, certName)
+			err = isCertRevoked(cert, nextCert, proxyURL, certName)
 			if err != nil {
-				log.Printf("Warning: %s failed to verify revocation status: %s Trying the next chain if there is one.\n", certName, err.Error())
+				log.Printf("Warning: %s failed to verify revocation status: %s. Trying the next chain if there is one.\n", certName, err.Error())
 				break
 			} else {
 				log.Printf("%s: OK.\n", certName)
 			}
 		}
 	}
-	return ErrOCSPNotVerified
+	log.Println("Certificate Validation Failed.")
+	return ErrNotValidated
 }
 
-// Check if a cert is revoked. If there are multiple OCSP servers, it is considered valid if any OCSP server can validate it.
-func isCertRevoked(cert *x509.Certificate, issueCert *x509.Certificate, proxyFunc ProxyFunc, dryRun bool, certName string) error {
-	var OCSPTimeOut time.Duration
-	if dryRun {
-		OCSPTimeOut = ocspSoftFailTimeOut
-	} else  {
-		OCSPTimeOut = ocspHardFailTimeOut
+// Check if a cert is revoked. It is considered valid if 1) CRL check passes 2) CRL is empty and OCSP check passes
+func isCertRevoked(cert *x509.Certificate, issuer *x509.Certificate, proxyURL *url.URL, certName string) error {
+	// CRL check; multiple CRL points for a single cert is possible; we only check the first one for now
+	for _, CDP := range cert.CRLDistributionPoints {
+		crl, err := fetchCRL(CDP, proxyURL)
+		if err != nil {
+			return err
+		}
+
+		err = issuer.CheckCRLSignature(crl)
+		if err != nil {
+			return err
+		}
+
+		for _, revoked := range crl.TBSCertList.RevokedCertificates {
+			if cert.SerialNumber.Cmp(revoked.SerialNumber) == 0 {
+				log.Println("Found a match on CRL. This certificate has been revoked.")
+				return errors.New("revoked")
+			}
+		}
+		// CRL check passed. Skipping OCSP checks.
+		return nil
 	}
 
-	// OCSP would fail if behind a TLS inspecting proxy
+	// If no CRL and no OCSP server, fail.
 	if len(cert.OCSPServer) == 0 {
 		return errors.New("no OCSP server found. This is likely a self-signed certificate used by a proxy or firewall for TLS inspection")
 	}
 
+	// multiple OCSP servers for a single cert is possible; we only check the first one for now
 	for _, ocspServer := range cert.OCSPServer {
-		err := func() error { // use an anonymous function so that the return value is easy to manager and defer() wouldn't be in a loop
-			client := &http.Client{
-				Transport: &http.Transport{
-					Proxy: proxyFunc,
-				},
-				Timeout: OCSPTimeOut,
-			}
-
-			buffer, err := ocsp.CreateRequest(cert, issueCert, &ocsp.RequestOptions{})
-			if err != nil {
-				return err
-			}
-
-			OCSPRequest, err := http.NewRequest(http.MethodPost, ocspServer, bytes.NewBuffer(buffer))
-			if err != nil {
-				return err
-			}
-			OCSPRequest.Header.Add("Content-Type", "application/ocsp-request")
-			OCSPRequest.Header.Add("Accept", "application/ocsp-response")
-
-			// no need to set basic auth again. Why?
-			response, err := client.Do(OCSPRequest)
-			if err, ok := err.(net.Error); ok && err.Timeout() { // a timeout error
-				return errors.New(fmt.Sprintf("connection timed out after %ds", OCSPTimeOut/time.Second))
-			} else if err != nil { // non-timeout error
-				return err
-			}
-
-			defer response.Body.Close()
-
-			output, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				return err
-			}
-
-			ocspResponse, err := ocsp.ParseResponse(output, issueCert)
-			if err != nil {
-				return err
-			}
-
-			if ocspResponse.Status == ocsp.Revoked {
-				return errors.New("revoked")
-			}
-
-			if ocspResponse.Status != ocsp.Good {
-				return errors.New("revocation status unknown")
-			}
-			return nil
-		}()
-		// check the returned err of above anonymous function
-		if err == nil{
-			return nil
-		} else if dryRun{
-			log.Printf("Warning: %s verification failed on %s: Error: %s. Ignore this error and continue in dry-run mode.\n", certName, ocspServer, err.Error())
-			return nil
-		} else{
-			log.Printf("Warning: %s verification failed on %s: Error: %s. Trying the next OCSP server if there is one.\n", certName, ocspServer, err.Error())
-		}
+		return checkOCSP(cert, issuer, ocspServer, proxyURL)
 	}
-	return errors.New("unable to verify on any OCSP server.")
+
+	// shouldn't ever reach here
+	return nil
+}
+
+// fetch and parse CRL
+func fetchCRL(url string, proxyURL *url.URL) (*pkix.CertificateList, error) {
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+		Timeout: gocspTimeOut,
+	}
+
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 300 {
+		return nil, errors.New("failed to retrieve CRL")
+	}
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	return x509.ParseCRL(bytes)
+}
+
+// check the OCSP status of a specific cert
+func checkOCSP(cert *x509.Certificate, issuer *x509.Certificate, ocspServer string, proxyURL *url.URL) error {
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+		Timeout: gocspTimeOut,
+	}
+
+	buffer, err := ocsp.CreateRequest(cert, issuer, &ocsp.RequestOptions{})
+	if err != nil {
+		return err
+	}
+
+	OCSPRequest, err := http.NewRequest(http.MethodPost, ocspServer, bytes.NewBuffer(buffer))
+	if err != nil {
+		return err
+	}
+	OCSPRequest.Header.Add("Content-Type", "application/ocsp-request")
+	OCSPRequest.Header.Add("Accept", "application/ocsp-response")
+
+	response, err := client.Do(OCSPRequest)
+	if err, ok := err.(net.Error); ok && err.Timeout() { // a timeout error
+		return errors.New(fmt.Sprintf("connection timed out after %ds", gocspTimeOut/time.Second))
+	} else if err != nil { // non-timeout error
+		return err
+	}
+
+	defer response.Body.Close()
+
+	output, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	ocspResponse, err := ocsp.ParseResponse(output, issuer)
+	if err != nil {
+		return err
+	}
+
+	if ocspResponse.Status == ocsp.Revoked {
+		return errors.New("revoked")
+	}
+
+	if ocspResponse.Status != ocsp.Good {
+		return errors.New("revocation status unknown")
+	}
+	return nil
 }
--- util/util.go
+++ util/util.go
@@ -1,15 +1,26 @@
 package util
 
 import (
+	"bufio"
+	"crypto/tls"
 	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
+	"net/http"
+	"net/url"
 )
 
-// a minimal implementation of SSL_CTX_load_verify_locations()
-// first search the certificates in CAFile, then those in CAPath
-func LoadVerifyLocations(CAFile string, CAPath string) (*x509.CertPool, error) {
+func PrettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
+
+func LoadCA(CAFile string) (*x509.CertPool, error) {
 	rootCAs, err := x509.SystemCertPool()
 	if err != nil {
 		rootCAs = x509.NewCertPool()
@@ -25,41 +36,58 @@ func LoadVerifyLocations(CAFile string, CAPath string) (*x509.CertPool, error) {
 		}
 		log.Printf("Added CA certificate %s.\n", CAFile)
 		return rootCAs, nil
-	} else if CAPath != "" {
-		files, err := ioutil.ReadDir(CAPath)
-		if err != nil {
-			return nil, err
-		}
-		appendAtLeastOneCA := false
-		for _, file := range files {
-			if file.IsDir() {
-				// do not go into subdirectory
-				continue
-			}
-			customCACert, err := ioutil.ReadFile(CAPath + "/" + file.Name())
-			if err != nil {
-				continue
-			}
-			if ok := rootCAs.AppendCertsFromPEM(customCACert); ok {
-				log.Printf("Added CA certificate %s to the bundle.\n", file.Name())
-				appendAtLeastOneCA = true
-			}
-		}
-
-		if appendAtLeastOneCA {
-			return rootCAs, nil
-		}
-
-		return nil, fmt.Errorf("could not read any certificate bundle in the provided CA path: %s", CAPath)
 	}
-	return nil, nil
+	return rootCAs, nil
 }
 
-// generate fibonacci sequence
-func Fibonacci() func() int {
-	x, y := 0, 1
-	return func() int {
-		x, y = y, x+y
-		return x
+// Initiate the ssl handshake through a proxy
+// step1. Establish HTTP tunnel with proxy via the CONNECT method
+// step2. Do TLS handshake
+func HandshakeProxy(server string, proxyURL *url.URL, tlsConfig *tls.Config) (err error) {
+	// get a TCP connection with the proxy
+	conn, err := net.Dial("tcp", proxyURL.Host)
+	if err != nil {
+		return
 	}
+	defer conn.Close()
+
+	// create a HTTP CONNECT request to remote server
+	req, err := http.NewRequest("CONNECT", fmt.Sprintf("https://%s", server), nil)
+	if err != nil {
+		return
+	}
+
+	// add proxy authentication
+	if proxyURL.User.String() != "" {
+		basicAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(proxyURL.User.String()))
+		req.Header.Add("Proxy-Authorization", basicAuth)
+	}
+
+	// send the CONNECT request to proxy to establish a tunnel
+	err = req.Write(conn)
+	if err != nil {
+		return
+	}
+
+	resp, err := http.ReadResponse(bufio.NewReader(conn), req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Printf("non-200 response status from proxy server: ", resp.Status, "\n")
+		return errors.New("non-200 status code returned from proxy")
+	}
+
+	log.Println("Succesfully established connection to proxy")
+	tlsConn := tls.Client(conn, tlsConfig)
+	defer tlsConn.Close()
+
+	err = tlsConn.Handshake()
+	if err != nil {
+		return
+	}
+
+	return
 }
